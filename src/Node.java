19dcae7232ddf453c749258bfd940adf3ea9feee
Kevin Zhang <kevin@Kevins-MBP.wlan>
2023-10-25 22:53:34 -0400

    Cleanup

--- src/Node.java
+++ src/Node.java
@@ -6,29 +6,12 @@ public class Node <T>{
 		this.data = data;
 		this.prev = null;
 		this.next = null;
-		
 	}
 
 	public Node(T data, Node<T> prev, Node <T> next) {
 		this.data = data;
 		this.prev = prev;
 		this.next = next;
-	}
-
-	public String showNext() {
-		if (this.next == null) {			
-			return "" + this.next;
-		}
-
-		return this.next + " | " + this.next.showNext();
-	}
-
-	public String showPrev() {
-		if (this.prev == null) {
-			return "" + this.prev;
-		}
-
-		return this.prev + " | " + this.prev.showPrev();
 	}
 
 	public String toString() {
